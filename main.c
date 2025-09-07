38b766a0624e71d05e0d12babbcf5c6a068e9d5a
lukas-sgx <lukas.soigneux@gmail.com>
2025-09-07 10:37:15 +0200

    Add color definitions and main program structure

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <colors.h>
+
+int scan(){
+    char buffer[250];
+    int c = 1;
+    
+    fgets(buffer, c, stdin);
+
+}
+
+int main(int argc, char const *argv[])
+{
+    int run = 1;
+    printf("");
+
+
+    while(run){
+        scan();
+    }
+
+    return 0;
+}
